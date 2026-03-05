d31d21b1ee462b1a1e2b6f53e49152b536fa39a6
aldoRastrelli <aldanarastrelli@gmail.com>
2026-03-05 16:31:42 -0300

    Remove branding header from sidebar

--- CodeStation/Sidebar/SidebarView.swift
+++ CodeStation/Sidebar/SidebarView.swift
@@ -3,46 +3,21 @@ import SwiftUI
 struct SidebarView: View {
     private enum Constants {
         static let buttonPadding: CGFloat = 8
-        static let logoSize: CGFloat = 32
-        static let nameHeight: CGFloat = 16
-        static let headerSpacing: CGFloat = 8
-        static let headerPadding: CGFloat = 12
     }
 
     @Bindable var viewModel: AppViewModel
 
     var body: some View {
-        VStack(spacing: 0) {
-            // App branding header
-            HStack(spacing: Constants.headerSpacing) {
-                Image("AppLogo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: Constants.logoSize)
-
-                Image("AppName")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: Constants.nameHeight)
-
-                Spacer()
+        List(selection: $viewModel.selectedEnvironmentID) {
+            ForEach(viewModel.sortedEnvironments) { env in
+                SidebarRowView(environment: env, viewModel: viewModel)
+                    .tag(env.id)
             }
-            .padding(.horizontal, Constants.headerPadding)
-            .padding(.vertical, Constants.headerPadding)
-
-            Divider()
-
-            List(selection: $viewModel.selectedEnvironmentID) {
-                ForEach(viewModel.sortedEnvironments) { env in
-                    SidebarRowView(environment: env, viewModel: viewModel)
-                        .tag(env.id)
-                }
-                .onMove { source, destination in
-                    viewModel.moveEnvironment(from: source, to: destination)
-                }
+            .onMove { source, destination in
+                viewModel.moveEnvironment(from: source, to: destination)
             }
-            .listStyle(.sidebar)
         }
+        .listStyle(.sidebar)
         .safeAreaInset(edge: .bottom) {
             HStack {
                 Button {
