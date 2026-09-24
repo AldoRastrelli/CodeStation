c2fe8ba2fef91c44bf6ed94c233d4c5e360fe531
aldoRas-vb <aldana@visualblasters.com>
2026-09-24 10:36:21 -0300

    Test empty cell drop handling

--- CodeStationTests/EmptyCellViewSnapshotTests.swift
+++ CodeStationTests/EmptyCellViewSnapshotTests.swift
@@ -10,4 +10,38 @@ final class EmptyCellViewSnapshotTests: XCTestCase {
         controller.view.frame = NSRect(x: 0, y: 0, width: 200, height: 150)
         assertSnapshot(of: controller, as: .image)
     }
+
+    func testAcceptDropForwardsSessionID() {
+        let expected = UUID()
+        var received: UUID?
+        let view = EmptyCellView(onAdd: {}, onSessionDropped: { id in
+            received = id
+            return true
+        })
+
+        XCTAssertTrue(view.acceptDrop([expected.uuidString]))
+        XCTAssertEqual(received, expected)
+    }
+
+    func testAcceptDropReturnsHandlerResult() {
+        let view = EmptyCellView(onAdd: {}, onSessionDropped: { _ in false })
+        XCTAssertFalse(view.acceptDrop([UUID().uuidString]))
+    }
+
+    func testAcceptDropRejectsInvalidPayload() {
+        var called = false
+        let view = EmptyCellView(onAdd: {}, onSessionDropped: { _ in
+            called = true
+            return true
+        })
+
+        XCTAssertFalse(view.acceptDrop([]))
+        XCTAssertFalse(view.acceptDrop(["not-a-uuid"]))
+        XCTAssertFalse(called)
+    }
+
+    func testAcceptDropWithoutHandlerReturnsFalse() {
+        let view = EmptyCellView(onAdd: {})
+        XCTAssertFalse(view.acceptDrop([UUID().uuidString]))
+    }
 }
